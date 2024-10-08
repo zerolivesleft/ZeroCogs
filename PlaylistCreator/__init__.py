714b87c46f0ed590f649c7b82aa887d418a52f79
zerolivesleft <dsrzero@gmail.com>
2024-10-08 13:04:44 -0500

    fix slash command removal

--- PlaylistCreator/__init__.py
+++ PlaylistCreator/__init__.py
@@ -4,4 +4,12 @@ async def setup(bot):
     cog = PlaylistCreator(bot)
     await bot.add_cog(cog)
     if hasattr(bot, "tree"):
+        try:
+            bot.tree.remove_command("playlist")
+        except:
+            pass
         bot.tree.add_command(cog.playlist)
+
+async def teardown(bot):
+    if hasattr(bot, "tree"):
+        bot.tree.remove_command("playlist")
