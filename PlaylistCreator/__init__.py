7ba4939022e9596bad764f2e8ebd096500881c65
zerolivesleft <dsrzero@gmail.com>
2024-10-08 13:07:42 -0500

    Revert "simple app"
    This reverts commit 029d1825a91cb2b9afc58d6e21df1f14b4c9bc25.

--- PlaylistCreator/__init__.py
+++ PlaylistCreator/__init__.py
@@ -1,7 +1,14 @@
 from .PlaylistCreator import PlaylistCreator
 
 async def setup(bot):
-    await bot.add_cog(PlaylistCreator(bot))
+    cog = PlaylistCreator(bot)
+    await bot.add_cog(cog)
+    if hasattr(bot, "tree"):
+        try:
+            bot.tree.remove_command("playlist")
+        except:
+            pass
+        bot.tree.add_command(cog.playlist)
 
 async def teardown(bot):
     if hasattr(bot, "tree"):
